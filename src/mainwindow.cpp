317eeddfb1a0a7a93bb3abb0e7c1b9914ba87cd5
Ben-Kaniobi <Ben_Kaniobi@gmx.net>
2015-01-06 17:54:21 +0100

    Implemented coffee cycle

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,6 +4,7 @@
 #include "mainwindow.hpp"
 #include "ui_mainwindow.h"
 
+#include "unistd.h"
 #include "hwhelper.hpp"
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -20,7 +21,46 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_Start_clicked()
 {
+    /* Disable gui elements */
+    this->ui->checkBox_Milch->setDisabled(true);
+    this->ui->checkBox_Zucker->setDisabled(true);
+    this->ui->pushButton_Start->setDisabled(true);
+    /* Force GUI refresh to make changes visible */
+    qApp->processEvents();
 
+    /* Output: Coffee (takes 3s) */
+    Set_LED2();
+    usleep(3000*1000);
+    Clear_LED2();
+
+    if(this->ui->checkBox_Milch->isChecked())
+    {
+        /* Output: Milk (takes 2s) */
+        Set_LED3();
+        usleep(2000*1000);
+        Clear_LED3();
+    }
+
+    if(this->ui->checkBox_Zucker->isChecked())
+    {
+        /* Output: Sugar (takes 0.5s) */
+        Set_LED4();
+        usleep(500*1000);
+        Clear_LED4();
+    }
+
+    for(int i=1; i<=3; i++)
+    {
+        Set_LED1();
+        usleep(500*1000);
+        Clear_LED1();
+        usleep(500*1000);
+    }
+
+    /* Enable gui elements */
+    this->ui->checkBox_Milch->setDisabled(false);
+    this->ui->checkBox_Zucker->setDisabled(false);
+    this->ui->pushButton_Start->setDisabled(false);
 }
 
 void MainWindow::closeEvent(QCloseEvent *event)
